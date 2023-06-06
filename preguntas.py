6959586f7f605db845f327fa15b5a432d21e531f
jruedat@unal.edu.co <jruedat@unal.edu.co>
2023-06-06 11:51:30 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -57,7 +57,7 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return tbl0.groupby(tbl0._c1).mean()["_c2"]
+    return = tbl0.groupby('_c1')['_c2'].mean()
 
 
 def pregunta_05():
